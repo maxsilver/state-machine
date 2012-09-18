7d48c12f976432027a6e7319d6b0e5262025641e
Matthew Seeley <matthew@threadlight.com>
2012-09-18 13:08:55 -0400

    #2 - Matching Single Characters

--- lib/reggie.rb
+++ lib/reggie.rb
@@ -2,5 +2,21 @@
 # The regular expression parser.
 
 class Reggie
+  def initialize(pattern)
+    @pattern = pattern[1..-2] # everything inside of / and /
+  end
 
+  def =~(str)
+    sindex = 0
+
+    loop do
+      if str[sindex] == @pattern
+        return sindex
+      end
+      sindex += 1
+
+      return nil if sindex > str.length
+    end
+
+  end
 end
--- spec/reggie_spec.rb
+++ spec/reggie_spec.rb
@@ -1,7 +1,38 @@
 require 'spec_helper'
 
 describe Reggie do
+
+  # Lesson 1 - It should exist
   it "should exist" do
     Reggie.should be
   end
+  # End Lesson 1
+
+  # Lesson 2 - Matching Single Characters
+  describe "matching single characters" do
+    describe "/a/" do
+      it "matches 'a'" do
+        regex = Reggie.new("/a/")
+        match = regex =~ "a"
+        match.should eq(0)
+      end
+
+      it "doesn't match 'b'" do
+        regex = Reggie.new("/a/")
+        match = regex =~ "b"
+        match.should eq(nil)
+      end
+    end
+
+    describe "/b/" do
+      it "matches 'b'" do
+        regex = Reggie.new("/b/")
+        match = regex =~ "abed"
+        match.should eq(1)
+      end
+    end
+  end
+  # End #2 - Matching Single Characters
+
+
 end
